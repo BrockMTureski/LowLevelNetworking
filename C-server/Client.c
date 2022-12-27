862b7557ce1610b465cad76e5e8340ab4ef71212
Brock <18bmt1@queensu.ca>
2022-12-26 22:26:22 -0500

    initial commit

---
+++ C-server/Client.c
@@ -0,0 +1,72 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <netdb.h>
+
+
+#define SERVER_PORT 80      //common HTTP port
+
+#define MAXLINE 4096        //max buffer length
+#define SA struct sockaddr
+
+
+void err_n_die(const char *fmt, ...); //variadic error handling function
+
+
+int main(int argc, char **argv){
+
+    int sockfd, n, sendbytes;
+    struct sockaddr_in servaddr;
+    char sendline[MAXLINE],recvline[MAXLINE];
+
+    //ensure correct number of args input
+    if(argc != 2) err_n_die("usage: %s <server address>",argv[0]);
+
+    //create new socket, check if created properly, handle it if not
+    //SOCK_STREAM used as this is TCP implementation, last arg 0 is for TCP also
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0))<0) err_n_die("Error creating the socket!");
+    
+    bzero(&servaddr, sizeof(servaddr)); //zero out servaddr
+    servaddr.sin_family = AF_INET; //internet address
+    servaddr.sin_port = htons(SERVER_PORT); //changes port byte order from host set to network standard
+
+    //converts string bound ip addr to binary 
+    if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr)<=0) err_n_die("inet_pton error for %s", argv[1]);
+
+    //connect to server
+    if(connect(sockfd, (SA *) &servaddr, sizeof(servaddr))<0) err_n_die("connect failed!");
+
+    //at this point we are connected to the server
+    //create line to send to server
+    sprintf(sendline, "GET / HTTP/1.1\r\n\r\n");
+    sendbytes=strlen(sendline);
+
+    //write req to server
+    if(write(sockfd,sendline,sendbytes)!=sendbytes) err_n_die("error writing request!");
+
+    //prepare memory space for server response by setting entire mem space to 0
+    memset(recvline,0,MAXLINE);
+
+    //read server response.
+    while((n=read(sockfd,recvline,MAXLINE-1))>0){
+    //print response
+    printf("%s",recvline);
+    //clear memory for response again
+    memset(recvline,0,MAXLINE);
+    } if(n<0)err_n_die("error reading response!");
+
+    exit(0);
+
+}
+
+
